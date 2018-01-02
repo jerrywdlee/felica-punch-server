5eeb2ef29d1c5d9d1d59fcd764936dc91d138e50
Jerry Lee <lzjjerrywoodylee@gmai.com>
2018-01-03 01:45:13 +0900

    update db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :punch_logs
   resources :cards
   resources :users
   resources :admins
+  resources :punch_logs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -17,7 +17,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { description: @user.description, email: @user.email, pass: @user.pass } }
+      post users_url, params: { user: { description: @user.description, email: @user.email, name: @user.name, pass: @user.pass } }
     end
 
     assert_redirected_to user_url(User.last)
@@ -34,7 +34,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { description: @user.description, email: @user.email, pass: @user.pass } }
+    patch user_url(@user), params: { user: { description: @user.description, email: @user.email, name: @user.name, pass: @user.pass } }
     assert_redirected_to user_url(@user)
   end
 
