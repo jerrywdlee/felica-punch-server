40c609a5871ad1fdb182bd50632e28e41ddef62d
Jerry Lee <lzjjerrywoodylee@gmai.com>
2018-01-02 20:35:58 +0900

    change build packs

--- nvm.sh
+++ nvm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install 8
+# nvm install 8
 
 
