eefd98abd14e32b4dd5d75015ae6a60b55875724
Jerry Lee <lzjjerrywoodylee@gmai.com>
2018-01-07 22:33:40 +0900

    update search

--- app/models/punch_log.rb
+++ app/models/punch_log.rb
@@ -6,13 +6,40 @@ class PunchLog < ApplicationRecord
   end
 
   def self.search(params)
+    user_id = params.dig(:User, :user_id)
+    time_span = params.dig(:PunchLog, :time_span)
     case true
-    when params.key?(:card_uid)
-      PunchLog.log_card_user.where('cards.card_uid = ?', params[:card_uid])
-    when params.key?(:name)
-      PunchLog.log_card_user.where('users.name = ?', params[:name])
+    # when params.key?(:card_uid)
+    #   PunchLog.log_card_user.where('cards.card_uid = ?', params[:card_uid])
+    # when params.key?(:name)
+    #   PunchLog.log_card_user.where('users.name = ?', params[:name])
+    when !user_id.to_s.empty? && !time_span.to_s.empty?
+      time_start = DateTime.parse(time_span).beginning_of_month
+      time_end = DateTime.parse(time_span).end_of_month
+      PunchLog.log_card_user
+      .where('users.id = ?', params.dig(:User, :user_id))
+      .where(created_at: time_start...time_end)
+    when !user_id.to_s.empty?
+      PunchLog.log_card_user.where('users.id = ?', params.dig(:User, :user_id))
+    when !time_span.to_s.empty?
+      time_start = DateTime.parse(time_span).beginning_of_month
+      time_end = DateTime.parse(time_span).end_of_month
+      PunchLog.log_card_user
+      .where(created_at: time_start...time_end)
     else
       PunchLog.log_card_user
     end
+  end
+
+  def self.time_span
+    time_start = PunchLog.all.first.created_at.localtime.beginning_of_month
+    time_end = PunchLog.all.last.created_at.localtime.end_of_month
+    time_span = []
+    while time_start < time_end
+      # time_span << { :time_start => time_start, :label => time_start.strftime("%Y-%m") }
+      time_span << [time_start.strftime("%Y-%m"), time_start]
+      time_start = time_start.next_month.beginning_of_month
+    end
+    return time_span.reverse
   end
 end
